aee34716cc30673186693227cd4ebb0d6cf34985
Endered <yy56ga10ve@gmail.com>
2022-09-25 16:39:11 +0900

    add example

---
+++ examples/call-cc.scm
@@ -0,0 +1,19 @@
+(define cont)
+
+(transpiler-ffi
+ "
+function myprint(cont,x)
+  print(x)
+end
+"
+ )
+
+(myprint
+ (+ 1 2 3
+    (call/cc (lambda (k)
+	       (set! cont k)
+	       (+ 10 (k 10000))))))
+
+(cont 0)
+(cont 1)
+(cont 2)
